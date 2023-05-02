b6f36daf4fa8ebb6926ce128d93eb3eb6b9f5592
María <frm@fernandowirtz.com>
2023-05-02 18:26:11 +0200

    first commit

---
+++ src/main/java/es/teis/oracle/util/ConnectionFactory.java
@@ -0,0 +1,14 @@
+package es.teis.oracle.util;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import oracle.jdbc.datasource.impl.OracleDataSource;
+
+public class ConnectionFactory {
+
+	
+	
+}
