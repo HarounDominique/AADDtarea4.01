3ee750fe9bdb5be97f1beec4e01ee5615104f9b1
HarounDominique <haroundominique@gmail.com>
2023-05-02 20:44:36 +0200

    first commit

--- src/main/java/es/teis/oracle/util/ConnectionFactory.java
+++ src/main/java/es/teis/oracle/util/ConnectionFactory.java
@@ -9,6 +9,19 @@ import oracle.jdbc.datasource.impl.OracleDataSource;
 
 public class ConnectionFactory {
 
+	private static OracleDataSource ods= null;
+	private static final String RUTA_FICHERO = "src/main/resources/db.properties";
+	private static final String URL_KEY = "url";
+	private static final String USER_KEY = "user";
+	private static final String PWD_KEY ="pwd";
 	
+	private ConnectionFactory() {
+		
+	}
 	
+	public OracleDataSource getDataSource() {
+		
+		return ods;
+		
+	}
 }
