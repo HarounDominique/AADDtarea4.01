7e7c66e7b8a43874871e8e079a53204752b6c6f9
HarounDominique <haroundominique@gmail.com>
2023-05-04 21:10:17 +0200

    versión definitiva

--- src/main/java/es/teis/oracle/util/ConnectionFactory.java
+++ src/main/java/es/teis/oracle/util/ConnectionFactory.java
@@ -9,19 +9,42 @@ import oracle.jdbc.datasource.impl.OracleDataSource;
 
 public class ConnectionFactory {
 
-	private static OracleDataSource ods= null;
+	private static OracleDataSource ods = null;
 	private static final String RUTA_FICHERO = "src/main/resources/db.properties";
 	private static final String URL_KEY = "url";
 	private static final String USER_KEY = "user";
-	private static final String PWD_KEY ="pwd";
-	
+	private static final String PWD_KEY = "pwd";
+
 	private ConnectionFactory() {
-		
+
 	}
-	
-	public OracleDataSource getDataSource() {
-		
+
+	public static OracleDataSource getDataSource() {
+		if (ods == null) {
+
+			try {
+				Properties props = new Properties();
+				FileInputStream fis = new FileInputStream(RUTA_FICHERO);
+				props.load(fis);
+				fis.close();
+
+				String url = props.getProperty(URL_KEY);
+				String user = props.getProperty(USER_KEY);
+				String pwd = props.getProperty(PWD_KEY);
+
+				ods = new OracleDataSource();
+				ods.setURL(url);
+				ods.setUser(user);
+				ods.setPassword(pwd);
+
+			} catch (FileNotFoundException e) {
+				System.err.println("No se encontró el archivo: " + e.getMessage());
+			} catch (IOException e) {
+				System.err.println("No se pudo leer el archivo: " + e.getMessage());
+			} catch (Exception e) {
+				System.err.println("No se pudo crear la conexión: " + e.getMessage());
+			}
+		}
 		return ods;
-		
 	}
 }
